05ac87d35c6de60ceaf02ae68351e68daed8e484
Prabhat Sharma <hi.prabhat@gmail.com>
2023-08-17 14:08:11 -0700

    upgraded and removed jaeger exporter (#6)
    * upgraded and removed jaeger exporter
    
    * update go and buildx version
    
    * removed duplicate

--- builder.sh
+++ builder.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 GO111MODULE=on go install go.opentelemetry.io/collector/cmd/builder@latest
-builder --config=manifest.yaml --name="otelcol" --version="0.0.1" --output-path=/tmp/dist
+builder --config=manifest.yaml --name="otelcol" --version="0.0.3" --output-path=/tmp/dist
 
  
